f3aa82230b71081527071085a3459bfb94bc8cfb
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-18 14:56:32 -0400

    03.py-Write a function full_name(first, last) that takes first name and last name as parameters and returns a single string in the format First Last.

---
+++ Function_questions/03.py
@@ -0,0 +1,9 @@
+#Write a function full_name(first, last) that takes first name and last name as parameters and returns a single string in the format "First Last".
+def full_name(first, last):
+    return first + " " + last
+
+first = input("Enter the first name: ")
+last = input("Enter the first name: ")
+
+a = full_name(first, last)
+print(a)
