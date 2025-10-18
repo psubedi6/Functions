6d74fc4f4733333b2904429458c256bda7e8d6d4
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-18 17:59:38 -0400

    07.py-Write a recursive function factorial(n) that returns the factorial of a number.

---
+++ Function_questions/07.py
@@ -0,0 +1,9 @@
+#Write a recursive function factorial(n) that returns the factorial of a number.
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = int(input("Enter a number: "))
+print("Factorial of", num, "is: ", factorial(num))
