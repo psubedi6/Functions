6e6cc49c13a1cb32f9c98b30d23fb559a2167e3b
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-18 18:33:30 -0400

    08.py- Write a recursive function sum_of_digits(n) that returns the sum of all digits of a given number.

--- Function_questions/07.py
+++ Function_questions/07.py
@@ -1,9 +1,10 @@
 #Write a recursive function factorial(n) that returns the factorial of a number.
+
 def factorial(n):
     if n==0 or n==1:
         return 1
     else:
         return n * factorial(n-1)
 
-num = int(input("Enter a number: "))
-print("Factorial of", num, "is: ", factorial(num))
+n = int(input("Enter a number: "))
+print("Factorial of", n, "is: ", factorial(n))
